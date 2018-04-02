4be830f3dc755d913afbd7698d120a5b8681e07f
annablgkv <annablgkv@gmail.com>
2018-04-02 18:52:19 +0300

    front page srvices end

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,32 @@ function owlHome() {
   })
 }
 
+function owlHome() {
+  $('.owlServicesSerti').owlCarousel({
+    nav: true,
+    dots: false,
+    margin: 50,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      700: {
+        items: 3
+      },
+      800: {
+        items: 5
+      }
+    } // end! responsive
+  })
+}
+
 function magicForm() {
   $('#magicFormTabs a').on('click', function (e) {
     e.preventDefault()
